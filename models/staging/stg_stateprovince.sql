7ea9808e203bc475bf95cd26e4246b1bda3a0678
rodolfolucastattoostudio@gmail.com <rodolfolucastattoostudio@gmail.com>
2026-06-26 01:14:02 +0000

    Adição das sources (conuntryregion e stateprovince) no arquivo sources.yml; Adição das respectivas arquivos staging.

---
+++ models/staging/stg_stateprovince.sql
@@ -0,0 +1,10 @@
+with source_stateprovince as (
+    select * from {{ source('raw', 'person_stateprovince') }}
+)
+
+select
+    cast(stateprovinceid as int) as id_estado_provincia,
+    cast(stateprovincecode as string) as codigo_estado,
+    cast(name as string) as nome_estado,
+    cast(countryregioncode as string) as codigo_regiao_pais
+from source_stateprovince
